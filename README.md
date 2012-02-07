d07f2ceb2dca8c0da7b8b1389db86101bbf12b25
Jonathan Cutrer <joncutrer@gmail.com>
2012-02-07 07:29:13 -0600

    more cleanup

--- README.md
+++ README.md
@@ -6,21 +6,18 @@ CakeSpec is a tool to get a CakePHP Project up and running fast using an applica
 specification file written in JSON format.
 
 CakeSpec can...
-Pull CakePHP and plugins from git.  
-Configure filesystem permissions
-Create PHP classes
-Set configuration variables
-Create your configuration files
-databases.php
-core.php
-routes.php
-run bake commands
-and much more. 
+* Pull CakePHP and plugins from git.  
+* Configure filesystem permissions
+* Create PHP classes
+* Set configuration variables
+* Create your configuration files
+  * databases.php
+  * core.php
+  * routes.php
+* run bake commands
+* and much more. 
 
-To use the tool you will need to first create a cakespec file in JSON format.
-
-In this stage of development you can find some example spec files in the tests/data
-directory.
+To use the tool you will need to first create a cakespec file in JSON format.  In this stage of development, the spec files structure may change slightly before reach beta.   You can get a sneak peak of a spec file by looking in the tests/data directory.
 
 Usage:
 
--- bootstrap.php
+++ bootstrap.php
@@ -9,20 +9,23 @@ define('CAKESPEC_DIR', dirname( __FILE__ ) );
 define('CAKESPEC_INDENT', '    ' );
 define('CAKESPEC_TAGLINE', 'This file was generated using CakeSpec (http://www.cakespec.com)' );
 
-require_once( CAKESPEC_DIR . DS . 'libraries/functions.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/functions.php');
 
 defined( 'CAKESPEC_GIT_EXEC' ) or define('CAKESPEC_GIT_EXEC', exec('which git') );
 defined( 'CAKESPEC_SVN_EXEC' ) or define('CAKESPEC_SVN_EXEC', exec('which svn') );
 
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecBuilder.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecLint.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecFilesystem.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CGClass.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CGCakeClass.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CGCakeController.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CGCakeModel.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecAppConfig.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecAppPlugin.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecAppView.php');
-require_once( CAKESPEC_DIR . DS . 'libraries/CakeSpecAppWebroot.php');
+require_once( CAKESPEC_DIR . DS . 'Lib/ProTemplateCompiler.php');
+
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecBuilder.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecLint.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecFilesystem.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CGClass.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CGCakeClass.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CGCakeController.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CGCakeModel.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecAppConfig.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecAppPlugin.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecAppView.php');
+require_once( CAKESPEC_DIR . DS . 'Inc/CakeSpecAppWebroot.php');
+
 
